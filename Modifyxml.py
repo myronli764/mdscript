f96a0f765a08d9256db00f33ea4cebe947284b9e
Mingyang Li <143047987+myronli764@users.noreply.github.com>
2024-01-25 17:52:27 +0800

    Update xml_write.py

--- Modifyxml.py
+++ Modifyxml.py
@@ -1,4 +1,4 @@
-from utils.io.xml_parser import XmlParser
+import XmlParser
 import networkx as nx
 import numpy as np
 
@@ -42,77 +42,3 @@ def XmlWriter(xml:XmlParser,filename='modified.xml'):
         f'\n</{program}_xml>'
         )
     f.close()
-
-
-xml = XmlParser('z-top-new-1tiao.xml')
-XmlWriter(xml)
-bonds = xml.data['bond']
-types = xml.data['type']
-N = len(types)
-top = nx.Graph()
-top.add_nodes_from([(i,dict(type=types[i])) for i in range(N)])
-top.add_edges_from([(b[1],b[2],dict(bt=b[0])) for b in bonds])
-endA = []
-endAnei = {}
-nonendA = []
-endXZ = []
-endXZnei = {}
-for n in top.nodes:
-    node = top.nodes[n]
-    degree = nx.degree(top,n)
-    if node['type'] == 'A':
-        if degree == 1:
-            nei_n = list(top.neighbors(n))[0]
-            endA.append(n)
-            endAnei[n] = nei_n
-        else :
-            nonendA.append(n)
-            #print(degree)
-    if node['type'] in ['X','Z']:
-        if degree == 1:
-            endXZ.append(n)
-            nei_n = list(top.neighbors(n))[0]
-            endXZnei[n] = nei_n
-
-#print(endA)
-#print(endAnei)
-for n in endXZ:
-    node = top.nodes[n]
-    if node['type'] == 'Z':
-        top.nodes[n]['type'] = 'Z0'
-        top.edges[(endXZnei[n], n)]['bt'] = f'Z0-{top.nodes[endXZnei[n]]["type"]}'
-    if node['type'] == 'X':
-        top.nodes[n]['type'] = 'X0'
-        top.edges[(endXZnei[n], n)]['bt'] = f'X0-{top.nodes[endXZnei[n]]["type"]}'
-
-for n in endA:
-    #print(top.has_edge(n,endAnei[n]))
-    top.nodes[n]['type'] = 'A0'
-    top.edges[(endAnei[n],n)]['bt'] = f'A0-{top.nodes[endAnei[n]]["type"]}'
-    print(nx.degree(top,endAnei[n]))
-new_bonds = []
-bts = []
-for e in top.edges:
-    if top.edges[e]['bt'] == 'Z-A':
-        new_bonds.append(('A-Z', e[0], e[1]))
-        continue
-    new_bonds.append((top.edges[e]['bt'],e[0],e[1]))
-    if top.edges[e]['bt'] not in bts:
-        bts.append(top.edges[e]['bt'])
-print(bts)
-xml.data['bond'] = new_bonds
-new_types = []
-for n in top.nodes:
-    new_types.append(top.nodes[n]['type'])
-
-xml.data['type'] = new_types
-#print(new_types[16],new_types[17])
-XmlWriter(xml)
-
-
-
-
-
-
-#print(types)
-#print(bonds)
